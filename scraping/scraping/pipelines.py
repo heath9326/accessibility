6bbf499385feeb706c6ee3482507fd153d70460b
Evgeniya Seminenko <Heathwindart@gmail.com>
2023-12-02 21:24:49 +0300

    pipeline and db

--- scraping/scraping/pipelines.py
+++ scraping/scraping/pipelines.py
@@ -7,7 +7,36 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import sqlite3
+
+
+
+
+
+
+
+conn.commit()
+conn.close()
+
 
 class ScrapingPipeline:
+
+    def __int__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("scrapy_database.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS elements_tb""")
+        self.curr.execute("""create table elements_tb(element text, utl text)""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute("""insert into _tb values(?, ?, ?)""",(item['page_html']))
+        self.conn.commit()
